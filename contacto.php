78f55fe582cb0a5b46672a8d04c75710edb06c6b
JuliusMatadamas <julius3877@gmail.com>
2022-04-27 18:27:26 -0500

    Se crean las demas paginas del sitio.

---
+++ contacto.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Julio Cesar Matadamas Zaragoza">
+    <title>Covicrisa</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+<nav>
+    <!-- NAVBAR -->
+    <div class="navbar">
+        <span>&equiv;</span>
+
+        <div class="logo">
+            <a href="#">
+                <img src="images/logotipo_inverse.png" alt="">
+            </a>
+        </div>
+
+        <div class="nav-links">
+            <div class="sidebar-logo">
+                <a href="#">
+                    <img src="images/isotipo_inverse.png" alt="">
+                </a>
+                <span>
+                        &times;
+                    </span>
+            </div>
+
+            <ul class="links">
+                <li><a href="index.html">Negocio</a></li>
+                <li><a href="catalogo.php">Catálogo de productos</a></li>
+                <li><a href="cotizacion.php">Solicitud de cotización</a></li>
+                <li><a href="contacto.php">Contacto</a></li>
+                <li><a href="login.php">Acceso a usuarios</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<main>
+    <div class="content">
+        <h2>Contacto</h2>
+    </div>
+</main>
+
+<footer>
+    PW1-JUMZ
+</footer>
+
+<!-- JS -->
+<script src="js/main.js"></script>
+</body>
+</html>
